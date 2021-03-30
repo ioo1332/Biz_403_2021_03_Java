33cfd9961e88677227b1215fe629454273e1e333
ioo1332 <ioo168400@gmail.com>
2021-03-31 00:09:50 +0900

    Service

--- Java_010_Application/src/com/callor/classes/Value_05.java
+++ Java_010_Application/src/com/callor/classes/Value_05.java
@@ -15,7 +15,7 @@ public class Value_05 {
 		// Integer result=vs.retNum;
 		
 		// retNum 변수는 ValueSErvice의 맴버 변수이다
-		// 이 맴버 변수를 외부에서 ㄱ밧을 변경하는것은
+		// 이 맴버 변수를 외부에서 값을 변경하는것은
 		// 코드를 매우 위험스럽게 만드는것이다
 		
 		// 좋은 방법은 맴버변수를 private로 설정하고
